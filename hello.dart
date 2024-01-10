4169a136b140031a802beae8232edaab82ea9f33
obay-it <info.obaymichael@gmail.com>
2024-01-10 16:00:38 +0100

    variables

--- hello.dart
+++ hello.dart
@@ -1,5 +1,19 @@
 
 void main() {
 	
-	print("hello world");
+	// variables
+
+	// strings
+	// var name = "john";
+	String name = "john";
+
+
+	// int 
+	// var x = 10;
+	int x = 41;
+
+	// Dynamic 'lorsque l'on ne connais pas le type de donner et que l'on ne souhaite pas tapper var'
+	dynamic y = 7;
+
+	print(y);
 }
